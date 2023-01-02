1d8787af78fd6ce3f5268a9aa76cf8cbf2a505c2
SamueleBartolucci <116120171+SamueleBartolucci@users.noreply.github.com>
2023-01-02 09:53:49 +0100

    Create ForEachExtensions.cs

---
+++ FluentExtensions/FluentExtensions/ForEach/ForEachExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCoding
+{
+    public static class ForEachExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach (var item in items)
+                action(item);
+        }
+
+        public static IEnumerable<K> ForEach<T, K>(this IEnumerable<T> items, Func<T, K> function)
+        {
+            var results = new List<K>();
+            foreach (var item in items)
+                results.Add(function(item));
+
+            return results;
+        }
+    }
+}
